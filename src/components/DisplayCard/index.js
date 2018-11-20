a375a63a27bf14c7beb11fcd795fae5f6881e21d
rkurupati <rajagopal_kurupati@cat.com>
2018-11-20 12:10:17 +0530

    Updated the logic of favourites

--- src/components/DisplayCard/index.js
+++ src/components/DisplayCard/index.js
@@ -6,19 +6,27 @@ export default class DisplayCard extends React.Component {
 
     constructor(props) {
         super(props);
+        this.bookMarkItem = this.bookMarkItem.bind(this);
+    }
+
+    bookMarkItem(e){
+        e.stopPropagation();
+        this.props.updateFavourite(this.props.idx, this.props.data);
     }
     
     render() {
-    	// alert(this.props);
-    	const {name, imageUrl, category} = this.props.data;
+    	const {name, imageUrl, category, isFavourite} = this.props.data;
         return (
-            <div className="display-card">
-            	<h1>{name}</h1>
-            	<img src="https://smedia2.intoday.in/lallantop/wp-content/uploads/2016/05/cute-dog_160516-023453.jpg" className="display-image" />
-            	<div>
-            		<span>Category</span>
-            		<span>{category}</span>
-            	</div>
+            <div className="display-card col-md-3">
+                <div className="card">
+                    <span className={isFavourite ? 'star book-marked' : 'star star-icon'} onClick={this.bookMarkItem}></span>
+                	<h1>{name}</h1>
+                	<img src={imageUrl} className="display-image" />
+                	<div>
+                		<span>Category: </span>
+                		<span>{category}</span>
+                	</div>
+                </div>
             </div>
         )
     }
--- src/containers/LandingPage/index.js
+++ src/containers/LandingPage/index.js
@@ -13,6 +13,24 @@ import _ from 'lodash';
 export default class LandingPage extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      petsData: [{
+                    "name": "Snoopy",
+                    "category": "Dog",
+                    "imageUrl": "https://smedia2.intoday.in/lallantop/wp-content/uploads/2016/05/cute-dog_160516-023453.jpg",
+                    "isFavourite": false
+                  },
+                  {
+                    "name": "Tommy",
+                    "category": "Cat",
+                    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg6mKUF2r_U-_Nz10Wnk4TB5NLniqvO3oUHJW3at3cJp5UzffsMg",
+                    "isFavourite": true
+                  }],
+      isFavouriteScreen: false
+    };
+    this.updateFavourite = this.updateFavourite.bind(this);
+    this.showFavourites = this.showFavourites.bind(this);
+    this.showAll = this.showAll.bind(this);
   }
 
   // getSnapshotBeforeUpdate(prevProps, prevState) {
@@ -27,9 +45,9 @@ export default class LandingPage extends React.Component {
     // Executed post render 
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    // return false to not render when any state is changed. Default return is true
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // return false to not render when any state is changed. Default return is true
+  // }
 
   componentDidCatch(error, info){
     // executed if any error occurs
@@ -39,18 +57,38 @@ export default class LandingPage extends React.Component {
     // this.props.fetchAllData({apiUrl: apiUrls.petsData, type: FETCH_DATA});
   }
 
+  updateFavourite(idx, data){
+    let {petsData} = this.state;
+    data.isFavourite = !data.isFavourite;
+    petsData[idx] = data;
+    this.setState({
+      petsData: petsData
+    })
+  }
+
+  showFavourites(){
+    this.setState({isFavouriteScreen: true});
+  }
+
+  showAll(){
+    this.setState({isFavouriteScreen: false});
+  }
+
   render() {
+    let petsData = this.state.isFavouriteScreen ? this.state.petsData.filter((data) => { return data.isFavourite; }) : this.state.petsData;
       return (
         <div className="landing-page">
-         { 
-            _.map([{
-                    "name": "Snoopy",
-                    "category": "dog",
-                    "imageUrl": ""
-                  }], (data) => {
-               return <DisplayCard data={data} />;
-             })
-          }
+          <div className="col-md-3">
+            <button className="col-md-12" onClick={this.showFavourites}>View Favourites</button>
+            <button className="col-md-12" onClick={this.showAll}>View All</button>
+          </div>
+          <div className="col-md-9">
+            { 
+              _.map(petsData, (data, k) => {
+                 return <DisplayCard data={data} key={k} updateFavourite={this.updateFavourite} idx={k}/>;
+               })
+            }
+          </div>
         </div>
       );
   }
